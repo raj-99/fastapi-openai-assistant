169337ef7cfca9c13d21e67bab76e5b8f4d15a5c
Rachit Raj <rachiraj@gmail.com>
2026-01-31 19:34:37 +0530

    main + router

---
+++ app/main.py
@@ -0,0 +1,18 @@
+import logging
+from fastapi import FastAPI
+from app.core.config import settings
+from app.core.logging import setup_logging
+from app.routes.answer import router as answer_router
+
+setup_logging()
+logger = logging.getLogger("main")
+
+app = FastAPI(title=settings.app_name)
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+app.include_router(answer_router, prefix="/api")
+
+logger.info(f"Started application: {settings.app_name} || environment = {settings.environment} ")
