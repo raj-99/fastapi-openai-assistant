12d7ad8750d9403fd6899d11933db4d6358d2900
Rachit Raj <rachiraj@gmail.com>
2026-03-01 16:32:55 +0530

    pgVector+ingest endpoint

--- app/core/db.py
+++ app/core/db.py
@@ -10,5 +10,10 @@ def get_db_connection():
         user=settings.db_user,
         password=settings.db_password
     )
+    with conn.cursor() as cur:
+        cur.execute("CREATE EXTENSION IF NOT EXISTS vector;")
+        create_table_query = """CREATE TABLE IF NOT EXISTS documents (id TEXT PRIMARY KEY,source TEXT,chunk_index INT NOT NULL,content TEXT NOT NULL,metadata JSONB,embedding VECTOR(1536));"""
+        cur.execute(create_table_query)
+    conn.commit()
     register_vector(conn)
     return conn
--- app/main.py
+++ app/main.py
@@ -1,16 +1,23 @@
 import logging
 import uuid
 from fastapi import FastAPI, Request
+from fastapi.concurrency import asynccontextmanager
 from app.core.config import settings
 from app.core.logging import setup_logging
 from app.routes.answer import router as answer_router
 from app.routes.ingest import router as ingest_router
+# from app.core.db import init_db
 
 setup_logging()
 logger = logging.getLogger("main")
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     logger.info("Initializing database...")
+#     init_db()
+#     yield
 
-app = FastAPI(title=settings.app_name)
-
+# app = FastAPI(title=settings.app_name, lifespan=lifespan)
+app = FastAPI(title=settings.app_name)    
 @app.middleware("http")
 async def add_request_id(request: Request, call_next):
     request_id = request.headers.get("x-request-id") or str(uuid.uuid4())
